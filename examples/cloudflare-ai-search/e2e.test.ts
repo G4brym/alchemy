f801fdd8cdecd909cc714825fc83cd55e9d7fa35
John Royal <34844819+john-royal@users.noreply.github.com>
2026-02-16 13:32:56 -0500

    improve e2e

--- examples/cloudflare-ai-search/e2e.test.ts
+++ examples/cloudflare-ai-search/e2e.test.ts
@@ -3,23 +3,32 @@ import assert from "node:assert";
 import { poll } from "../../alchemy/src/util/poll";
 
 export async function test(props: { url: string }) {
+  console.log("testing", props.url);
   await poll({
     description: "wait for worker to be ready",
     fn: () => fetch(props.url),
     predicate: (res) => res.ok,
   });
-  const url = new URL(props.url);
-  url.pathname = "/query";
-  url.searchParams.set("q", "What is the capital of France?");
-  const response = await poll({
-    description: "wait for search to be ready",
-    fn: () => fetch(url),
+  console.log("worker ready");
+  await poll({
+    description: "wait for index ready",
+    fn: () => {
+      const url = new URL(props.url);
+      url.pathname = "/search";
+      url.searchParams.set("q", "What is the capital of France?");
+      console.log(url.href);
+      return fetch(url);
+    },
     predicate: (res) => {
       console.log(res);
       return res.ok;
     },
-    timeout: 5000,
   });
+  console.log("index ready");
+  const url = new URL(props.url);
+  url.pathname = "/query";
+  url.searchParams.set("q", "What is the capital of France?");
+  const response = await fetch(url);
   const result = (await response.json()) as AutoRagAiSearchResponse;
   assert(
     result.response.includes("Paris"),
@@ -27,4 +36,5 @@ export async function test(props: { url: string }) {
   );
   assert(result.data.length > 0, "No data returned");
   assert.equal(result.data[0].filename, "france.txt");
+  console.log("success");
 }
